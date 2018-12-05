2174a52db13b200f0fb0004fafe58c95337ad170
Roman Kreuzhuber <rkreuzh@gmail.com>
2018-12-05 15:39:51 +0000

    add missing file

---
+++ ebrowse/epi_annotation.py
@@ -0,0 +1,143 @@
+
+chip_seq = {'bed': {
+                'CD4': {
+                    'Segmentation': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/regulatory_regions/reg_build_ensembl90/hg19/CD4.bed',
+                    'ATAC': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/ATACseq/CD4/TotalCD4/SLX11600ATACAd006_A_t6.fseqPeaks.bed',
+                    'H3K27ac': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/H3K27ac/allPeaks/CD4_resting.bed',
+                    'SE_const': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/H3K27ac/superenhancerConstituents/CD4_resting_SEConstituents.bed',
+                    'SE': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/SE/annot/CD4NonActive_Merged_1e-5_broad_peaks_annot.bed',
+                    },
+                'MK': {
+                    'ATAC': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/plt_natcomms_paper/bp_epigenomes_data_share/MK.ATAC.peaks.bed',
+                    'H3K27ac': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/H3K27ac/allPeaks/MK.bed',
+                    'SE_const': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/H3K27ac/superenhancerConstituents/MK_SEConstituents.bed',
+                    'SE': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/SE/annot/MK_merged_1e-5_broad_peaks_annot.bed',
+                    'Segmentation': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/eQTLHIC/data/blueprint/MK_states_annotatedcollapsed_colors.bed',
+                    'GATA1': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/tfbs_plt/devCell-GATA1-hg19.bed',
+                    'FL1': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/tfbs_plt/devCell-FLI1-hg19.bed',
+                    'MEIS1': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/tfbs_plt/MEIS1_primary_min30tags_peaks.bed',
+                    'RUNX1': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/tfbs_plt/devCell-RUNX1-hg19.bed',
+                    'SCL': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/tfbs_plt/devCell-SCL-hg19.bed',
+                    'GATA2': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/tfbs_plt/devCell-GATA2-hg19.bed',
+                    },
+                'CMP': {'ATAC': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/plt_natcomms_paper/open_chromatin/20160205_SLX-10203_S01GAC72_SLX-10205_S01H7E72_CMP_Fresh_ATACSeq_overlapping_merged.bed'},
+                'CD8': {'Segmentation': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/regulatory_regions/reg_build_ensembl90/hg19/CD8.bed',
+                        'H3K27ac': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/h3k27ac/annot/CD8positiveAlphaBetaTcell_merged_1e-5_broad_peaks_annot.bed',
+                        'SE': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/SE/annot/CD8positiveAlphaBetaTcell_merged_1e-5_broad_peaks_annot.bed'},
+                'CD15': {'Segmentation': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/regulatory_regions/reg_build_ensembl90/hg19/CD15.bed',
+                         'H3K27ac': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/h3k27ac/annot/Neutrophils_Merged_1e-5_broad_peaks_annot.bed',
+                         'SE': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/SE/annot/Neutrophils_Merged_1e-5_broad_peaks_annot.bed'},
+                'CD19': {
+                    'CTCF': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/CTCF/CTCF-BCell_peaks.narrowPeak',
+                    'ATAC': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/ATACseq/NaiveBcell/GSE71338SRR2126769_A_t6.fseqPeaks.bed',
+                    'H3K27ac': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/H3K27ac/allPeaks/CD19_NaiveBcell.bed',
+                    'SE_const': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/H3K27ac/superenhancerConstituents/CD19_NaiveBcell_SEConstituents.bed',
+                    'SE': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/SE/annot/NaiveBcell_merged_1e-5_broad_peaks_annot.bed',
+                    'Segmentation': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/regulatory_regions/reg_build_ensembl90/hg19/CD19.bed',
+                    },
+                'mac': {'Segmentation': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/regulatory_regions/reg_build_ensembl90/hg19/mac.bed',
+                        'H3K27ac': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/h3k27ac/annot/Macrophage_merged_1e-5_broad_peaks_annot.bed',
+                        'SE': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/SE/annot/Macrophage_merged_1e-5_broad_peaks_annot.bed'},
+                'CD14': {
+                    'CTCF': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/CTCF/CTCF-CD14pMono_peaks.narrowPeak',
+                    'ATAC': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/ATACseq/Monocytes/SLX11599ATACAd012_A_t6.fseqPeaks.bed',
+                    'H3K27ac': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/H3K27ac/allPeaks/Monocytes.bed',
+                    'SE_const': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/H3K27ac/superenhancerConstituents/Monocytes_SEConstituents.bed',
+                    'SE': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/SE/annot/Monocytes_merged_1e-5_broad_peaks_annot.bed',
+                    'Segmentation': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/regulatory_regions/reg_build_ensembl90/hg19/CD14.bed',
+                    },
+                'MEP': {'ATAC': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/plt_natcomms_paper/open_chromatin/20160205_SLX-10203_S01GAC71_SLX-10205_S01HQD71_MEP_Fresh_ATCSeq_overlapping_merged.bed'},
+                'HSC': {'ATAC': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/plt_natcomms_paper/open_chromatin/20160205_SLX-10203_S01GD671_SLX-10205_S01GV773_HSC_Fresh_ATACSeq_overlapping_merged.bed'},
+                'EB': {'ATAC': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/plt_natcomms_paper/open_chromatin/20160205_SRR1182654_SRR1182656_EB_Kellis_DNaseI_overlapping_merged.bed'},
+                },
+            'bw': {
+                # 'CD19': {'CTCF': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/CTCF/CTCF-BCell.bw',
+                #          'ATAC': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/ATACseq/NaiveBcell/GSE71338SRR2126769_A_50.fseqWig.bw'},
+                # 'CD4': {'ATAC': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/ATACseq/CD4/TotalCD4/SLX11600ATACAd006_A_50.fseqWig.bw'},
+                # 'MK': {
+                #     'ATAC': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/plt_natcomms_paper/bp_epigenomes_data_share/MK.ATAC.bw',
+                #     'H3K27ac': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/plt_natcomms_paper/bp_epigenomes_data_share/MK.H3K27ac.bw',
+                #     'H3K4me1': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/plt_natcomms_paper/bp_epigenomes_data_share/MK.H3K4me1.bw',
+                #     'DHS': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/plt_natcomms_paper/bp_epigenomes_data_share/MK.DNaseI.bw',
+                #     'CTCF': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/plt_natcomms_paper/bp_epigenomes_data_share/MK.CTCF.bw',
+                #     'MEIS1': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/plt_natcomms_paper/MEIS1_primary_e120v3nbs_unique.bw',
+                #     },
+                # 'CD14': {'CTCF': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/CTCF/CTCF-CD14pMono.bw',
+                #          'ATAC': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/regulome/ATACseq/Monocytes/SLX11599ATACAd012_A_50.fseqWig.bw'
+                #     },
+                }
+            }
+
+
+rna_seq = {'bw': {'PLT': {'RNAseq': '/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/reference/plt_natcomms_paper/6SP-Plt-RNAseq.sorted.bam.bw'}}}
+
+cell_type_equiv = [{"MK", "PLA", "PLT"}, {"CD4"}, {"CD8"}, {"CD14"}, {"CD15"}, {"CD19"}, {"mac", "MAC"}]
+natural_ordering = ["rnaseq", "tfbs", "histone", "open_chrom", "segmentation", "superenhancers"]
+
+track_categories = {'ATAC':"open_chrom", 'CTCF':"tfbs", 'DHS':"open_chrom", 'FL1':"tfbs", 'GATA1':"tfbs",
+                    'GATA2':"tfbs", 'H3K27ac':"histone", 'H3K4me1':"histone", 'MEIS1':"tfbs", 'RNAseq':"rnaseq",
+                    'RUNX1':"tfbs", 'SCL':"tfbs", 'SE':"superenhancers", 'SE_const':"superenhancers",
+                    'Segmentation':"segmentation"}
+
+def igv_type_relabelling(type_in):
+    relabel = {"bw": "wig"}
+    if type_in in relabel:
+        return relabel[type_in]
+    return type_in
+
+def get_all_cats():
+    cat = []
+    for ddir in [chip_seq, rna_seq]:
+        for ftype in ddir:
+            for cell_type in ddir[ftype]:
+                cat.extend(ddir[ftype][cell_type].keys())
+    return sorted(list(set(cat)))
+
+def fpath_rebase_rel(fpath):
+    return fpath.replace("/nfs/research1/stegle/users/rkreuzhu/webapp_data/epi/", "")
+
+
+def get_tracks(cell_type, root_path):
+    cell_type_equivs = list([e for e in cell_type_equiv if cell_type in e][0])
+    #
+    sel_tracks = {}
+    for ftype in ["bw", "bed"]:
+        for ct in chip_seq[ftype]:
+            if ct not in cell_type_equivs:
+                continue
+            for track_name in chip_seq[ftype][ct]:
+                cat = track_categories[track_name]
+                if cat not in sel_tracks:
+                    sel_tracks[cat] = {}
+                # First track of a kind wins
+                if track_name not in sel_tracks[cat]:
+                    fpath = fpath_rebase_rel(chip_seq[ftype][ct][track_name])
+                    sel_tracks[cat][track_name] = [fpath, ftype, ct]
+    #
+    ftype = "bw"
+    for ct in rna_seq[ftype]:
+        if ct not in cell_type_equivs:
+            continue
+        for track_name in rna_seq[ftype][ct]:
+            cat = track_categories[track_name]
+            if cat not in sel_tracks:
+                sel_tracks[cat] = {}
+            # First track of a kind wins
+            if track_name not in sel_tracks[cat]:
+                fpath = fpath_rebase_rel(rna_seq[ftype][ct][track_name])
+                sel_tracks[cat][track_name] = [fpath, ftype, ct]
+    # re-sorting:
+    sorted_tracks = []
+    for cat in natural_ordering:
+        if cat in sel_tracks:
+            for track_name in sorted(sel_tracks[cat]):
+                path, ftype, orig_ct = sel_tracks[cat][track_name]
+                track_height = 50
+                if ftype == "bed":
+                    track_height = 35
+                sorted_tracks.append({"name": orig_ct +" "+track_name, "url" : root_path + "/" + path,
+                                      "height": track_height, "type":igv_type_relabelling(ftype)})
+    return sorted_tracks
+
+def get_celltypes():
+    return ["PLT", "CD4", "CD8", "CD14", "CD15", "CD19", "MAC"]
